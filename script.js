c0dfc27de219cfb12aecaa5ba668cfa88945df16
Jaine09 <jainejesus0911@gmail.com>
2025-03-17 10:21:52 -0300

    página de login com redirecionamente e validação de usuário

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var txtEmailAdm = document.getElementById('txtEmail').value;
-var txtSenhaAdm = document.getElementById('txtSenha').value;
+function acionarEntrarAdm(event) {
+    event.preventDefault();
+    var txtEmailAdm = document.getElementById('txtEmail').value;
+    var txtSenhaAdm = document.getElementById('txtSenha').value;
 
-function acionarEntrarAdm(){
-    if(txtEmailAdm == "admin@gmail.com" && txtSenhaAdm == "admin123"){
-        alert("Usuário correto");
-    }else{
+   
+
+    if (txtEmailAdm == "admin@gmail.com" && txtSenhaAdm == "admin123") {
+        alert("usuario correto");
+        window.location.href = "./homepage.html";
+    } else {
+
         alert("Usuário incorreto");
     }
 }
